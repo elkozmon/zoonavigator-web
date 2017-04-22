8ac850127185349ceb34e594c526679a9ef7f36e
Lubos Kozmon <lubosh91@gmail.com>
2017-04-22 19:12:21 +0200

    Push Docker image without tag as well

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-DOCKER_IMAGE="$DOCKER_USERNAME/$DOCKER_REPOSITORY:$1"
+DOCKER_IMAGE="$DOCKER_USERNAME/$DOCKER_REPOSITORY"
+DOCKER_TAG="$1"
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" && \
   docker build -t $DOCKER_IMAGE . && \
-  docker push $DOCKER_IMAGE
+  docker push $DOCKER_IMAGE && \
+  docker tag $DOCKER_IMAGE $DOCKER_IMAGE:$DOCKER_TAG && \
+  docker push $DOCKER_IMAGE:$DOCKER_TAG
