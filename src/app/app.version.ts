3aa0208f2e0b15084c51a168bc7b342fa745c8ed
Lubos Kozmon <lubosh91@gmail.com>
2018-11-01 17:44:18 +0100

    Set version to 0.5.2-dev.1

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on 2018-11-01T17:40:02+01:00
-export const APP_VERSION = "0.5.1";
+// This file was generated on 2018-11-01T17:44:18+01:00
+export const APP_VERSION = "0.5.2-dev.1";
