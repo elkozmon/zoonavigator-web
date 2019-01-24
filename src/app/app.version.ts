53f4c8eb5e880e8f759c7d082d00f52bece2ae80
Lubos Kozmon <lubosh91@gmail.com>
2019-01-24 02:24:51 +0100

    Set version to 0.6.1-dev.1

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on 2019-01-24T02:19:31+01:00
-export const APP_VERSION = "0.6.0";
+// This file was generated on 2019-01-24T02:24:51+01:00
+export const APP_VERSION = "0.6.1-dev.1";
