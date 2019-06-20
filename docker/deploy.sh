694382c3684229ab95fbe1dc10e4ba6064a1ca42
elkozmon <contact@elkozmon.com>
2019-06-20 22:26:55 +0200

    Switch to recommended stdin password input

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,6 +2,6 @@
 
 DOCKER_TAG="$1"
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" \
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin \
   && docker build -t $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG . \
   && docker push $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
