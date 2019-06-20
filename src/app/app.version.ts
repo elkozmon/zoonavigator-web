7e6e0d963eabfd2263eb0a3ac382d2971802d3c1
elkozmon <contact@elkozmon.com>
2019-06-20 23:37:32 +0200

    Set version to 0.6.2

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on 2019-04-13T12:19:57+02:00
-export const APP_VERSION = "0.6.2-dev.1";
+// This file was generated on 2019-06-20T23:37:32+02:00
+export const APP_VERSION = "0.6.2";
